1f56e1f0fc519cb919235ea81fb18a69b019b752
mimoon5860 <mahmodulislammoon@gmail.com>
2022-03-26 13:18:21 +0600

    single users ads api created and get multiple ads api created

--- controllers/adsControllers.js
+++ controllers/adsControllers.js
@@ -60,3 +60,43 @@ exports.getSingleAd = (req, res) => {
         }
     })
 }
+
+
+// get multi ads 
+exports.getMultiAds = (req, res) => {
+    const count = req.params.count;
+    const state = req.query.state;
+    const city = req.query.city;
+    console.log({ count, state, city })
+    const query = `select id, title, created_at from ads where state="${state}" and city="${city}" and status="pending" limit ${count}`;
+
+    db.query(query, (err, rows, fields) => {
+        if (err) {
+            throw new Error(err.message);
+        }
+        if (rows.length) {
+            res.status(200).send(rows);
+        } else {
+            res.status(200).send("No Ads Found");
+        }
+    })
+}
+
+
+
+// Get single Users All Ads
+exports.getUsersAds = (req, res) => {
+    const user_id = req.params.id;
+    const query = `select id, title, created_at, status from ads where u_id=${user_id}`;
+
+    db.query(query, (err, rows, fields) => {
+        if (err) {
+            throw new Error(err.message);
+        }
+        if (rows.length) {
+            res.status(200).send(rows);
+        } else {
+            res.status(200).send("No Ads Found");
+        }
+    })
+}
--- routes/ads.routes.js
+++ routes/ads.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getSingleAd, createAd } = require('../controllers/adsControllers');
+const { getSingleAd, createAd, getMultiAds, getUsersAds } = require('../controllers/adsControllers');
 const multiFile = require('../middleware/fileUpload/multiFile');
 const router = express.Router();
 
@@ -8,6 +8,12 @@ router.post('/create/ad', multiFile, createAd);
 
 // Get Single Ad route 
 router.get('/single/ad/:id', getSingleAd);
+
+// get multiple ads
+router.get('/multi/ads/:count', getMultiAds);
+
+// get single users ads
+router.get('/user/:id', getUsersAds);
 
 
 module.exports = router;
